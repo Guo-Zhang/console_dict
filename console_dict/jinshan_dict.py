8b479eb4e63b7e5715a9a33b7a9c744189d99080
Guo ZHANG <iguo19961110@foxmail.com>
2017-04-04 17:22:36 +0800

    update

--- console_dict/jinshan_dict.py
+++ console_dict/jinshan_dict.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 import requests
+# from vlc import MediaPlayer
 from xtls.colorful import Color,colorful_print
 
 
@@ -14,6 +15,7 @@ def get_data(word):
     url = "http://dict-co.iciba.com/api/dictionary.php?type=json&w=%s&key=%s"%(word, KEY)
     r = requests.get(url)
     data = r.json()
+    # print(data)
     return data
 
 
@@ -21,9 +23,16 @@ def print_means(parts):
     for part in parts:
         colorful_print(" %s %s"%(part['part'],';'.join(part['means'])),Color.BLUE)
 
+
+def read_pron(url):
+    p = MediaPlayer(url)
+    p.play()
+
+
 def print_data(data):
     colorful_print("%s |%s|"%(data['word_name'],data['symbols'][0]['ph_am']),Color.RED)
     print_means(data['symbols'][0]['parts'])
+    # read_pron(data['symbols'][0]['ph_am_mp3'])
 
 
 def jinshan_dict(word):
