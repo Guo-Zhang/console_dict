fd9bbb1dfbe94a0b51c3d55dd5985ac6638e11a1
Guo ZHANG <iguo19961110@foxmail.com>
2017-04-04 11:15:54 +0800

    origin

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
+
 setup(
     name='console_dict',
     version='0.0.1.dev1',
     packages=['console_dict'],
-    url='github.com/Guo-Zhang/guodict',
+    url='github.com/Guo-Zhang/console_dict',
     license='MIT',
     author='Guo Zhang',
     author_email='zhangguo@stu.xmu.edu.cn',
